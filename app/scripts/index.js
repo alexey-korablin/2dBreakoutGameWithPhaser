ff978a8aa59b0e1df6a6fd5e317a02349753c81c
Alexey Korablin <Alexey_Korablin@epam.com>
2018-08-03 16:27:24 +0300

    001_breakout Initialize the framework

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,5 +1,19 @@
 ;(function() {
     'use strict';
 
-    console.log(`The script has been started at ${new Date()}`);
+    console.log(`${Phaser ? 'We are have the Phaser here' : 'Unfortunatelly we do not have Phaser here'}`);
+    console.log(Phaser);
+
+    function preload() {} // To preload an assets
+
+    function create() {} // Executed once when everything is loaded and ready
+
+    function update() {} // Executed on every frame.
+
+    const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
+        preload: preload, create: create, update: update
+    });
+
+    console.log(game);
+    
 }());
